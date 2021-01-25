82b3ceee6ec85dd01fb34c163fb8214341d0723c
Misagh <mm1844@gmail.com>
2021-01-25 16:46:36 +0400

    support json payloads for resources

--- inspektr-audit/src/main/java/org/apereo/inspektr/audit/AuditTrailManager.java
+++ inspektr-audit/src/main/java/org/apereo/inspektr/audit/AuditTrailManager.java
@@ -6,9 +6,9 @@
  * Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License.  You may obtain a
  * copy of the License at the following location:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -18,7 +18,9 @@
  */
 package org.apereo.inspektr.audit;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.apereo.inspektr.common.Cleanable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,7 +37,18 @@ import java.util.Set;
 public interface AuditTrailManager extends Cleanable {
     Logger LOG = LoggerFactory.getLogger(AuditTrailManager.class);
 
-    ObjectMapper MAPPER = new ObjectMapper().findAndRegisterModules();
+    ObjectMapper MAPPER = new ObjectMapper().findAndRegisterModules()
+        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true)
+        .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+    static String toJson(final Object arg) {
+        try {
+            return MAPPER.writeValueAsString(arg);
+        } catch (final Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return null;
+    }
 
     /**
      * Make an audit trail record. Implementations could use any type of back end medium to serialize audit trail
@@ -68,15 +81,18 @@ public interface AuditTrailManager extends Cleanable {
     }
 
     enum AuditFormats {
-        DEFAULT, JSON
-    }
-
-    static String toJson(final Object arg) {
-        try {
-            return MAPPER.writeValueAsString(arg);
-        } catch (final Exception e) {
-            LOG.error(e.getMessage(), e);
-        }
-        return null;
+        DEFAULT {
+            @Override
+            public String serialize(final Object object) {
+                return object.toString();
+            }
+        },
+        JSON {
+            @Override
+            public String serialize(final Object object) {
+                return AuditTrailManager.toJson(object);
+            }
+        };
+        public abstract String serialize(Object object);
     }
 }
